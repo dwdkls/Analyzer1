8dbf387b94e0fe1e8e4c226ca0627a1e03bfb64d
Dawid K <dwdkls@gmail.com>
2022-07-17 03:50:48 +0200

    Another approach to generator

--- Analyzer1/Analyzer1.CodeFixes/XmlDocumentationGenerator.cs
+++ Analyzer1/Analyzer1.CodeFixes/XmlDocumentationGenerator.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Analyzer1
 {
@@ -8,28 +11,95 @@ namespace Analyzer1
     {
         public static DocumentationCommentTriviaSyntax ForClassName(string className)
         {
-            XmlTextSyntax x1 = SyntaxFactory.XmlText("/// ");
-            //.WithTextTokens(
-            //     SyntaxFactory.TokenList(SyntaxFactory.XmlTextLiteral("/// ")));
-            //SyntaxFactory.TriviaList(SyntaxFactory.DocumentationCommentExterior("///")), " ", " ", SyntaxFactory.TriviaList())));
+            SyntaxTrivia sol = SyntaxFactory.DocumentationCommentExterior("///");
 
-            XmlElementSyntax x2 = SyntaxFactory.XmlElement("summary",
-                SyntaxFactory.SingletonList<XmlNodeSyntax>(SyntaxFactory.XmlText(className)));
-            //SyntaxFactory.XmlElementStartTag(SyntaxFactory.XmlName(SyntaxFactory.Identifier("summary"))),
-            //SyntaxFactory.XmlElementEndTag(SyntaxFactory.XmlName(SyntaxFactory.Identifier("summary"))))
-            //.WithContent(SyntaxFactory.SingletonList<XmlNodeSyntax>(
-            //    SyntaxFactory.XmlText().WithTextTokens(
-            //        SyntaxFactory.TokenList(
-            //        SyntaxFactory.XmlTextLiteral(SyntaxFactory.TriviaList(), className, className, SyntaxFactory.TriviaList())))));
+            XmlTextSyntax startOfLine = SyntaxFactory.XmlText("/// ");
+            XmlElementStartTagSyntax startOfSummary = SyntaxFactory.XmlElementStartTag(SyntaxFactory.XmlName("summary"));
+            XmlElementEndTagSyntax endOfSummary = SyntaxFactory.XmlElementEndTag(SyntaxFactory.XmlName("summary"));
+            XmlTextSyntax summaryContent = SyntaxFactory.XmlText(className);
 
-            XmlTextSyntax x3 = SyntaxFactory.XmlText(Environment.NewLine);
-                        //.WithTextTokens(SyntaxFactory.TokenList(
-                        //    SyntaxFactory.XmlTextNewLine(SyntaxFactory.TriviaList(), Environment.NewLine, Environment.NewLine, SyntaxFactory.TriviaList())));
 
-            DocumentationCommentTriviaSyntax testDocumentation = SyntaxFactory.DocumentationCommentTrivia(
-                SyntaxKind.SingleLineDocumentationCommentTrivia, SyntaxFactory.List(new XmlNodeSyntax[] { x1, x2, x3 }));
+            XmlTextSyntax cc = SyntaxFactory.XmlNewLine(className);
+
+            var scc = cc.ToFullString();
+
+            XmlElementSyntax yy = SyntaxFactory.XmlSummaryElement(summaryContent);
+
+            var syy = yy.ToFullString();
+
+            SyntaxToken uu = SyntaxFactory.XmlTextNewLine(className);
+
+            var suu = uu.ToFullString();
+
+            //var x2 = SyntaxFactory.XmlElement("summary",
+            //    SyntaxFactory.SingletonList<XmlNodeSyntax>(SyntaxFactory.XmlText(className)));
+
+            //SyntaxNode node;
+            //SyntaxToken token;
+
+
+            XmlTextSyntax newLine = SyntaxFactory.XmlText(Environment.NewLine);
+
+            //var allNodes = newLine.ChildNodes();
+            //var allTokens = newLine.ChildTokens();
+
+            //SyntaxFactory.XmlTextLiteral(
+
+            //var ugagawe = SyntaxFactory.TriviaList(SyntaxFactory.DocumentationCommentExterior("///"), SyntaxFactory.XmlTextNewLine();
+
+            //SyntaxFactory.XmlSummaryElement()
+
+            var testDocumentation = SyntaxFactory.DocumentationComment(
+                    SyntaxFactory.XmlElement("summary", 
+                        SyntaxFactory.List(new List<XmlNodeSyntax>(new[]{
+                            SyntaxFactory.XmlText("")
+                                .AddTextTokens(
+                                    SyntaxFactory.XmlTextNewLine(Environment.NewLine, true))
+                                        .AddTextTokens(
+                                                SyntaxFactory.XmlTextNewLine($"{summaryContent}{Environment.NewLine}", true))
+                                    })
+                                    )));
+
+
+            //DocumentationCommentTriviaSyntax testDocumentation = SyntaxFactory.DocumentationCommentTrivia(
+            //    SyntaxKind.MultiLineDocumentationCommentTrivia,
+            //    SyntaxFactory.List(new XmlNodeSyntax[] {
+            //        //startOfLine,
+            //        //yy
+            //        startOfLine, 
+            //        //startOfSummary,
+            //        newLine,
+            //        startOfLine, summaryContent, newLine,
+            //        startOfLine, 
+            //        //endOfSummary, 
+            //    })
+            //    //newLine.ChildTokens().First()
+            ////SyntaxFactory.XmlTextNewLine("", false)
+            //);
+
+            //var x = testDocumentation.EndOfComment.ToFullString();
 
             return testDocumentation;
         }
+
+        /* XmlTextSyntax x1 = SyntaxFactory.XmlText()
+                .WithTextTokens(SyntaxFactory.TokenList(
+                    SyntaxFactory.XmlTextLiteral(
+                        SyntaxFactory.TriviaList(SyntaxFactory.DocumentationCommentExterior("///")), " ", " ", SyntaxFactory.TriviaList())));
+
+            XmlElementSyntax x2 = SyntaxFactory.XmlElement(
+                            SyntaxFactory.XmlElementStartTag(SyntaxFactory.XmlName(SyntaxFactory.Identifier("summary"))),
+                            SyntaxFactory.XmlElementEndTag(SyntaxFactory.XmlName(SyntaxFactory.Identifier("summary"))))
+                        .WithContent(SyntaxFactory.SingletonList<XmlNodeSyntax>(
+                            SyntaxFactory.XmlText().WithTextTokens(SyntaxFactory.TokenList(
+                                SyntaxFactory.XmlTextLiteral(SyntaxFactory.TriviaList(), "test", "tost", SyntaxFactory.TriviaList())))));
+
+            XmlTextSyntax x3 = SyntaxFactory.XmlText()
+                        .WithTextTokens(SyntaxFactory.TokenList(
+                            SyntaxFactory.XmlTextNewLine(SyntaxFactory.TriviaList(), Environment.NewLine, Environment.NewLine, SyntaxFactory.TriviaList())));
+
+            DocumentationCommentTriviaSyntax testDocumentation = SyntaxFactory.DocumentationCommentTrivia(
+                SyntaxKind.SingleLineDocumentationCommentTrivia, SyntaxFactory.List(new XmlNodeSyntax[] { x1, x2, x3 }));
+        */
     }
 }
