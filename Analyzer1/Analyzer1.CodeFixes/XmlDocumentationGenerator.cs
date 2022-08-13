d98eef97720603c32c48aadd1bd65b7200a14d92
Dawid K <dwdkls@gmail.com>
2022-08-13 04:23:52 +0200

    Support for parameters

--- Analyzer1/Analyzer1.CodeFixes/XmlDocumentationGenerator.cs
+++ Analyzer1/Analyzer1.CodeFixes/XmlDocumentationGenerator.cs
@@ -63,7 +63,11 @@ public static class XmlDocumentationGenerator
         foreach (var item in declaration.ParameterList.ChildNodes())
         {
             var paramName = item.ToFullString();
-            var xmlElementSyntax = SyntaxFactory.XmlParamElement(paramName);
+
+            var paramDescription = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(ToSentence(paramName));
+            var paramDescXml = SyntaxFactory.XmlText($"{Article(paramDescription)} {paramDescription}");
+
+            var xmlElementSyntax = SyntaxFactory.XmlParamElement(paramName, paramDescXml);
 
             xmlNodes.Add(SyntaxFactory.XmlNewLine(Environment.NewLine));
             xmlNodes.Add(xmlElementSyntax);
@@ -76,10 +80,6 @@ public static class XmlDocumentationGenerator
             xmlNodes.Add(returns);
         }
 
-        //var documentation = returns != null
-        //    ? SyntaxFactory.DocumentationComment(summary, SyntaxFactory.XmlNewLine(Environment.NewLine), returns)
-        //    : SyntaxFactory.DocumentationComment(summary);
-
         var documentation = SyntaxFactory.DocumentationComment(xmlNodes.ToArray());
 
         return documentation.WithTrailingTrivia(SyntaxFactory.CarriageReturnLineFeed);
@@ -87,34 +87,29 @@ public static class XmlDocumentationGenerator
 
     private static XmlElementSyntax BuildReturns(TypeSyntax type)
     {
-        bool IsValidReturnType(string typeName) => typeName != null && typeName != VoidTypeName;
-
-        bool IsVowel(char c) => c != default && "aeiouAEIOU".IndexOf(c) >= 0;
-
-        string Article(string s) => IsVowel(s.FirstOrDefault()) ? "An" : "A";
-
-        if (!IsValidReturnType(type.ToString()))
+        if (type.ToString() == VoidTypeName)
         {
             return null;
         }
 
-        var typeAsString = GetTypeName(type);
+        var typeAsString = BuildTypeName(type);
 
         return SyntaxFactory.XmlReturnsElement(SyntaxFactory.XmlText($"{Article(typeAsString)} {typeAsString}."));
     }
 
-    private static string GetTypeName(TypeSyntax type)
+    private static string BuildTypeName(TypeSyntax type)
     {
         return type switch
         {
             PredefinedTypeSyntax predefined => predefined.Keyword.ValueText,
             IdentifierNameSyntax identifier => identifier.Identifier.ValueText,
             GenericNameSyntax generic =>
-                $"{generic?.Identifier.ValueText} of {string.Join(" and ", generic.TypeArgumentList.Arguments.Select(GetTypeName))}",
-            ArrayTypeSyntax arrayType => $"array of {GetTypeName(arrayType.ElementType)}",
+                $"{generic?.Identifier.ValueText} of {string.Join(" and ", generic.TypeArgumentList.Arguments.Select(BuildTypeName))}",
+            ArrayTypeSyntax arrayType => $"array of {BuildTypeName(arrayType.ElementType)}",
             _ => string.Empty,
         };
     }
+
 
     public static DocumentationCommentTriviaSyntax ForProperty(PropertyDeclarationSyntax declaration)
     {
@@ -128,8 +123,15 @@ public static class XmlDocumentationGenerator
             .WithTrailingTrivia(SyntaxFactory.CarriageReturnLineFeed);
     }
 
-    private static string ToSentence(string PascalString)
+    private static string Article(string s)
     {
-        return Regex.Replace(PascalString, "(?!^)([A-Z])", " $1");
+        bool IsVowel(char c) => c != default && "aeiouAEIOU".IndexOf(c) >= 0;
+
+        return IsVowel(s.FirstOrDefault()) ? "An" : "A";
+    }
+
+    private static string ToSentence(string s)
+    {
+        return Regex.Replace(s, "(?!^)([A-Z])", " $1");
     }
 }
